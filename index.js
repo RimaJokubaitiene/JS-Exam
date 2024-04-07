8b065ce6deabb3c5dd970cc0989ac2947e86f258
RJokubaitiene <rimaskucaite@gmail.com>
2024-04-07 22:56:37 +0300

    Title flex, pading

--- index.js
+++ index.js
@@ -29,3 +29,62 @@ function showTab(tabName) {
 
 // Lentelės validacija //
 
+/*function submitForm(event) {
+  event.preventDefault(); 
+
+  const firstNameInput = document.getElementById('first-name');
+  const lastNameInput = document.getElementById('last-name');
+  const phoneNumberInput = document.getElementById('phone-number');
+
+  const firstName = firstNameInput.value;
+  const lastName = lastNameInput.value;
+  const phoneNumber = phoneNumberInput.value;
+
+  if (!isNaN(firstName) || firstName.trim() === '') {
+    alert('First name should not be a number or empty!');
+    return false; 
+  }
+
+  if (!isNaN(lastName) || lastName.trim() === '') {
+    alert('Last name should not be a number or empty!');
+    return false; 
+  }
+
+  if (isNaN(phoneNumber) || phoneNumber.trim() === '' || phoneNumber.length !== 10) {
+    alert('Phone number should be a 10-digit number!');
+    return false; 
+  }
+
+  sendFormData(firstName, lastName, phoneNumber);
+
+  alert('Form submitted successfully!');
+  return true;
+}
+
+function sendFormData(firstName, lastName, phoneNumber) {
+
+}*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const submitButtons = document.querySelectorAll("input[type='submit']");
+
+  submitButtons.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      const form = button.closest("form");
+      const inputs = form.querySelectorAll("input[required]");
+      let isEmpty = false;
+
+      inputs.forEach(function (input) {
+        if (input.value.trim() === "") {
+
+        }
+      });
+
+      if (isEmpty) {
+        event.preventDefault();
+        alert("Please enter the requested data");
+        return false;
+      }
+    });
+  });
+}) ;
